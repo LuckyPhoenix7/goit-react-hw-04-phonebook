027c9aedeb6e1f40a7a8081628771f48a3430305
LuckyPhoenix7 <a.lubashov@gmail.com>
2023-11-23 23:01:42 +0100

    HW#4-phonebook v1.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,20 +6,20 @@ import { Filter } from './Filter/Filter';
 import { Layout, FirstTitle, SecondTitle } from './Layout';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter, setFilter] = useState('');
-  const [savedContacts, setSavedContacts] = useState(
-    localStorage.getItem('contacts')
-  );
+  const initialContacts = () => {
+    const savedContacts = localStorage.getItem('contacts');
+    return savedContacts
+      ? JSON.parse(savedContacts)
+      : [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ];
+  };
 
-  useEffect(() => {
-    setContacts(JSON.parse(savedContacts));
-  }, []);
+  const [contacts, setContacts] = useState(initialContacts());
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
